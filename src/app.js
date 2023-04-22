a444bbe19ab8e7483910d82f98d63a82ffe3a1e8
jgomez2011 <jesus.gomez0911@gmail.com>
2023-04-22 20:32:36 +0000

    excuse-generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
   console.log("Hello Rigo from the console!");
+  let excuse = document.querySelector("#excuse");
+  excuse.innerHTML = `${who[Math.floor(Math.random() * who.length)]} ${
+    what[Math.floor(Math.random() * what.length)]
+  } ${action[Math.floor(Math.random() * action.length)]} ${
+    when[Math.floor(Math.random() * when.length)]
+  }`;
 };
